afec352beca661850b48a22d17209e0c7f1e05cb
Panayiotis Charalambous <panayiotis@cyprus.com>
2026-01-24 10:49:27 +0100

    organised with header files

---
+++ HW1/problem8/board.c
@@ -0,0 +1,22 @@
+int** initboard() {
+    int **board = malloc(8 * sizeof(int*));
+
+    for (int i = 0; i < 8; i++) {
+        board[i] = malloc(8 * sizeof(int));
+        for (int j = 0; j < 8; j++) {
+            board[i][j] = 0;  // initialize values
+        }
+    }
+    return board;
+}
+
+void printboard(int **board){
+    printf("\n|---|---|---|---|---|---|---|---|\n");
+    for (int i = 0; i < 8; i++) {
+        printf("| ");
+        for (int j = 0; j < 8; j++) {
+            printf("%i | ", board[i][j]);
+        }
+        printf("\n|---|---|---|---|---|---|---|---|\n");
+    }
+}
--- HW1/problem8/queens.c
+++ HW1/problem8/queens.c
@@ -1,33 +1,10 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <stdio.h>
+#include "board.h"
+#include "safe.h"
 
 #define N 8
-
-int safe(int row, int col, int **board);
-
-int** initboard() {
-    int **board = malloc(8 * sizeof(int*));
-
-    for (int i = 0; i < 8; i++) {
-        board[i] = malloc(8 * sizeof(int));
-        for (int j = 0; j < 8; j++) {
-            board[i][j] = 0;  // initialize values
-        }
-    }
-    return board;
-}
-
-void printboard(int **board){
-    printf("\n|---|---|---|---|---|---|---|---|\n");
-    for (int i = 0; i < 8; i++) {
-        printf("| ");
-        for (int j = 0; j < 8; j++) {
-            printf("%i | ", board[i][j]);
-        }
-        printf("\n|---|---|---|---|---|---|---|---|\n");
-    }
-}
 
 int count = 0;
 void solve(int col, int **board){
@@ -50,23 +27,6 @@ void solve(int col, int **board){
     }
 
 }
-
-int safe(int row, int col, int **board){
-    // Check this row on left side
-    for(int j=col; j>=0; j--){
-        if(board[row][j] == 1) return 0;
-    }
-    // Check upper diagonal on left side
-    for(int i=row, j=col; i>=0 && j>=0; i--, j--){
-        if(board[i][j]==1) return 0;
-    }
-    // Check lower diagonal on left side
-    for(int i=row, j=col; i<N && j>=0; i++, j--){
-        if(board[i][j]==1) return 0;
-    }
-    return 1;
-}
-
 
 int main(){
     int **b = initboard();
