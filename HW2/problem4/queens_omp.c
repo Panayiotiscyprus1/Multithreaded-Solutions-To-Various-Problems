a81322841d8447d3639404ffc4cc1a07770df5b1
Marios Stavrou <marios.stavrou04@gmail.com>
2026-01-27 12:11:22 +0100

    starting logic

--- HW2/problem4/queens_omp.c
+++ HW2/problem4/queens_omp.c
@@ -1,6 +1,7 @@
-#include <stdlib.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <omp.h>
+
 #include "board.h"
 #include "safe.h"
 
@@ -8,21 +9,27 @@
 
 int count = 0;
 void solve(int col, int **board){
+    #pragma omp parallel
+    {
+        if(col == N){
+            #pragma omp critical
+            {
+                count++;
+                printf("\ncount: %i \n", count);
+                printboard(board);
+            }
+            return;
+        }
 
-    if(col == N){
-        count++;
-        printf("\ncount: %i \n", count);
-        printboard(board);
-        return;
-    }
+        #pragma omp parallel for
+        for(int row = 0; row < N; row++){
+            if (safe(row, col, board)){
+                board[row][col] = 1;
 
-    for(int row = 0; row < N; row++){
-        if (safe(row, col, board)){
-            board[row][col] = 1;
-
-            solve(col+1, board);
-            
-            board[row][col] = 0;
+                solve(col+1, board);
+                
+                board[row][col] = 0;
+            }
         }
     }
 
