cda3006775a547dc7d6145628f2fd3e97b42c497
Marios Stavrou <marios.stavrou04@gmail.com>
2026-02-03 17:53:08 +0100

    linux shi

--- Unisex-Bathroom-Problem/main.c
+++ Unisex-Bathroom-Problem/main.c
@@ -5,19 +5,6 @@
 #include <unistd.h>
 #include <time.h>
 
-#ifdef __APPLE__
-    #include <dispatch/dispatch.h>
-    typedef dispatch_semaphore_t sem_type;
-    #define SEM_WAIT(s)   dispatch_semaphore_wait(s, DISPATCH_TIME_FOREVER)
-    #define SEM_POST(s)   dispatch_semaphore_signal(s)
-    #define SEM_INIT(s, v) s = dispatch_semaphore_create(v)
-#else
-    #include <semaphore.h>
-    typedef sem_t sem_type;
-    #define SEM_WAIT(s)   sem_wait(&s)
-    #define SEM_POST(s)   sem_post(&s)
-    #define SEM_INIT(s, v) sem_init(&s, 0, v)
-#endif
 
 #define SHARED 0
 #define MEN 6
@@ -53,7 +40,7 @@ void *Men(void *arg){
         }else{
             sem_post(&q);
         }
-        
+
         nm++;
         if (dm>0){
             dm--;
