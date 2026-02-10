33c52496428696c061f4357e379a58daa0e55436
Marios Stavrou <marios.stavrou04@gmail.com>
2026-02-10 18:36:38 +0100

    me liky

--- Dining in Hell/Table.java
+++ Dining in Hell/Table.java
@@ -1,30 +1,30 @@
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
-public class Table{
+public class Table {
+    final int N = 5;
+    private final Queue<Integer> waitingQueue = new LinkedList<>();
 
-    final int N=5;
-
-    private Queue<Integer> waitingQueue = new LinkedList<>();
-
-    public void initTable(){
-        for(int i = 0; i < 5; i++) waitingQueue.add(i);
+    public void initTable() {
+        for (int i = 0; i < N; i++) waitingQueue.add(i);
     }
 
-    public synchronized Feed(int feeder){
-        
-        while(waitingQueue.peek() != feeder){
+    public synchronized int Feed(int feeder) throws InterruptedException {
+        while (waitingQueue.peek() != null && waitingQueue.peek() == feeder) {
             wait();
         }
-
-        waitingQueue.poll();
-        System.out.println("Thread " + feeder + "feeds thread " + target);
-        Thread.sleep(ThreadLocalRandom.current().nextInt(1000));
-
+        int target = waitingQueue.poll();   // take head (someone else)
+        notifyAll();                        // head changed -> wake waiters
+        return target;
     }
 
-    public synchronized Done(int feeder){
-        waitingQueue.add(feeder);
+    public void Feeding(int feeder, int target) throws InterruptedException {
+        System.out.println("Thread " + feeder + " feeds thread " + target);
+        Thread.sleep(ThreadLocalRandom.current().nextInt(1000));
+    }
+
+    public synchronized void Done(int target) {
+        waitingQueue.add(target);
         notifyAll();
     }
-
 }
