47f1e08e4d3a04132ae672805a04e51defa85d2a
Marios Stavrou <marios.stavrou04@gmail.com>
2026-02-10 17:22:34 +0100

    test

--- Dining in Hell/Table.java
+++ Dining in Hell/Table.java
@@ -1,4 +1,11 @@
 class Table{
     private boolean[] hungry = new boolean[5];
     private boolean[] beingFed = new boolean[5];
+
+    public synchronized requestToFeed(int personID){
+        if(beingFed[personID] == true){
+            wait();
+        }
+        
+    }
 }
