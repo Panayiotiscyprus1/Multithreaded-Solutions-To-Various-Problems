60ebab7d60efb0d45173054c2d81e8d49c5a46e3
Panayiotis Charalambous <panayiotis@cyprus.com>
2026-02-19 12:46:26 +0100

    no sync

--- Stable-Marriage-Problem/marriagermi/Man.java
+++ Stable-Marriage-Problem/marriagermi/Man.java
@@ -29,7 +29,7 @@ public class Man extends UnicastRemoteObject implements manI {
         return this.partnerId;
     }
 
-    public synchronized void onResponse(String response, int womanId) throws RemoteException {
+    public void onResponse(String response, int womanId) throws RemoteException {
         if (response.equals("ACCEPT")) {
             this.partnerId = womanId;
             Driver.trace("M" + id +" ENGAGED to W"+ womanId);
@@ -55,7 +55,7 @@ public class Man extends UnicastRemoteObject implements manI {
         }
     }
 
-    public synchronized void proposeNext() throws RemoteException {
+    public void proposeNext() throws RemoteException {
         proposalIndex++;
         if (proposalIndex >= preferences.length) return; 
         int wId = preferences[proposalIndex];
