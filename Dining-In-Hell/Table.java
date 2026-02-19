4ec4e50a46ebc936a20818fa86aa7e7ee35955e5
Panayiotis Charalambous <panayiotis@cyprus.com>
2026-02-19 18:13:37 +0100

    not pairs feeding each other minor randomized patend by a not so smart person :)

--- Dining-In-Hell/Table.java
+++ Dining-In-Hell/Table.java
@@ -3,6 +3,8 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Table {
     final int N = 6;
+    boolean b0 = false;
+    boolean b2 = false;
     private final Queue<Integer> waitingQueue = new LinkedList<>();
 
     public Table() {
@@ -11,6 +13,18 @@ public class Table {
 
     //called by feeder, return target to feed
     public synchronized int Feed(int feeder) throws InterruptedException {
+        if (feeder == 0){
+            while (b0 == false) {
+                wait();
+                b0 = !b0; // toggle b0 after being notified
+            }
+        }
+        if (feeder == 2){
+            while (b2 == false) {
+                wait();
+                b2 = !b2; // toggle b0 after being notified
+            }
+        }
         while (waitingQueue.peek() != null && waitingQueue.peek() == feeder) {
             wait();
         }
@@ -22,6 +36,8 @@ public class Table {
     //called by feeder, simulate feeding -> not synced to allow concurrent feeding
     public void Feeding(int feeder, int target) throws InterruptedException {
         System.out.println("Thread " + feeder + " feeds thread " + target);
+        if (feeder == 0) b0 = !b0; // toggle b0 after feeding
+        if (feeder == 2) b2 = !b2; // toggle b0 after feeding
         Thread.sleep(ThreadLocalRandom.current().nextInt(500, 9000));
     }
 
