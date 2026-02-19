98fd7d99c44e1775d409f646d9593157c9a8efea
Marios Stavrou <marios.stavrou04@gmail.com>
2026-02-19 11:03:21 +0100

    woman

--- Stable-Marriage-Problem/marriagermi/woman.java
+++ Stable-Marriage-Problem/marriagermi/woman.java
@@ -1,3 +1,60 @@
-public class woman {
-    
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class woman extends UnicastRemoteObject implements womanI {
+
+    private int id;
+    private int partnerId;
+    private int[] preferences = new int[5];
+    private int[] rank = new int[5];
+
+    public woman(int id) throws RemoteException {
+        // export object
+        super();
+        this.id = id;
+        this.partnerId = -1;
+        orderPref();
+    }
+
+    // Generate random preference order and ranking
+    public void orderPref() {
+        List<Integer> men = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            men.add(i);
+        }
+
+        Collections.shuffle(men);
+
+        for (int pos = 0; pos < 5; pos++) {
+            int manId = men.get(pos);
+            preferences[pos] = manId;
+            rank[manId] = pos;
+        }
+    }
+
+    public synchronized String onProposal(int manId) throws RemoteException {
+
+        if (partnerId == -1) {
+            partnerId = manId;
+            return "ACCEPT";
+        }
+
+        if (rank[manId] < rank[partnerId]) {
+            partnerId = manId;
+            return "DUMP";
+        }
+
+        return "REJECT";
+    }
+
+    public int getId() throws RemoteException {
+        return id;
+    }
+
+    public int getPartnerId() throws RemoteException {
+        return partnerId;
+    }
 }
--- Stable-Marriage-Problem/marriagermi/womanI.java
+++ Stable-Marriage-Problem/marriagermi/womanI.java
@@ -2,7 +2,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface womanI extends Remote {
-    String onProposal(String manId) throws RemoteException;
-    String getId() throws RemoteException;
-    String getPartnerId() throws RemoteException;
+    String onProposal(int manId) throws RemoteException;
+    int getId() throws RemoteException;
+    int getPartnerId() throws RemoteException;
 }
